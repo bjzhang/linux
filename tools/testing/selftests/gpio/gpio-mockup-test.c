2753a835add28219c6058fbf9ff84d1d561d2096
Bamvor Jian Zhang <bamvor.zhangjian@linaro.org>
2016-01-09 16:22:03 +0800

    in progress
    Signed-off-by: Bamvor Jian Zhang <bamvor.zhangjian@linaro.org>

--- tools/testing/selftests/gpio/gpio-mockup-test.c
+++ tools/testing/selftests/gpio/gpio-mockup-test.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>	//for system
 
 #define MAX_GC 5
 struct gpio_testcase_t {
@@ -42,6 +43,19 @@ struct gpio_testcase_t {
 //	gpio_test_fail "0,32,40,16,20,1"
 };
 
+struct gpio_chip {
+	char *name;
+	int base;
+	int ngpio;
+};
+
+struct gpio_group {
+	struct gpio_chip *chips;
+	struct gpio_chip* (*list)(struct gpio_group *groups);
+	int (*test)(struct gpio_chip *chip, unsigned pin);
+	int (*check)(struct gpio_chip *chip, unsigned pin);
+};
+
 //TODO: get sysfs, debugfs, location
 const char *gpio_sysfs = "/sys/class/gpio"
 const char *gpio_drv_sysfs = "/sys/devices/platform/gpio-mockup/gpio`
@@ -53,17 +67,20 @@ void prerequisite()
 	printf("uid<%d>, euid<%d>\n", getuid(), geteuid());
 }
 
-int gpio_test(struct gpio_testcase_t *tc)
+int gpio_test(const char *module, struct gpio_testcase_t *tc)
 {
 	int i;
 	int offset = 0;
-	char *insert_module = "/usr/sbin/modprobe -q gpio_mockup_bases=\"xxx,xxx,xxx,xxx,xxx\" gpio_mockup_ngpios=\"xxx,xxx,xxx,xxx,xxx\"";
-	char *bases = "xxx,xxx,xxx,xxx,xxx";
+	char *insert;
+	char *remove;
+	char *bases = "xxx,xxx,xxx,xxx,xxx";	//The max gpio is 512.
 	char *ngpios = "xxx,xxx,xxx,xxx,xxx";
+	int ret;
 
 	offset = 0;
 	for (i = 0; i < tc->nr_gc; i++){
 		if (i != tc->nr_gc - 1 )
+			asprintf
 			offset = snprintf(bases + offset, sizeof(bases), "%d,", tc->bases[i]);
 		else
 			offset = snprintf(bases + offset, sizeof(bases), "%d", tc->bases[i]);
@@ -86,41 +103,48 @@ int gpio_test(struct gpio_testcase_t *tc)
 	}
 
 	if (tc->gc_nr == 0)
-		snprintf(insert_module, strlen(insert_module), "/usr/sbin/modprobe -q");
+		ret = asprintf(&insert, "/usr/sbin/modprobe -q %s", module);
 	else
-		snprintf(insert_module, strlen(insert_module), "/usr/sbin/modprobe -q gpio_mockup_bases=\"%s\"gpio_mockup_ngpios=\"%s\"", bases, ngpios);
+		ret = asprintf(&insert, "/usr/sbin/modprobe -q %s gpio_mockup_bases=\"%s\"gpio_mockup_ngpios=\"%s\"", module, bases, ngpios);
 
-	system(insert_module);
+	if (ret < 0)
+		return -1;
+
+	//TODO: check whether range conflict with existing gpio drivers
+	system(insert);
 	//TODO error check
 
-	echo -n "GPIO $module test with base,ngpio configure<"
-	if [ X$param = X ]; then
-		echo "(default)>: ";
-	else
-		echo "$param>: "
-	fi
-	printf "%-10s %-5s %-5s\n" name base ngpio
-	gpiochip=`ls -d $GPIO_DRV_SYSFS/gpiochip* 2>/dev/null`
-	if [ X"$gpiochip" = X ]; then
-		echo "no gpiochip"
-	else
-		for chip in $gpiochip; do
-			name=`basename $chip`
-			base=`cat $chip/base`
-			ngpio=`cat $chip/ngpio`
-			printf "%-10s %-5s %-5s\n" $name $base $ngpio
-			if [ $ngpio = "0" ]; then
-				echo "number of gpio is zero is not allowed".
-			fi
-			test_one_pin $base
-			test_one_pin $(($base + $ngpio - 1))
-			test_one_pin $((( RANDOM % $ngpio )  + $base ))
-			if [ X$invalid_pin != X ]; then
-				test_one_pin_fail $invalid_pin
-			fi
-		done
-	fi
-	remove_module
+	struct gpio_group *groups_sysfs;
+	groups_sysfs->chips = groups_sysfs->list(groups_sysfs);
+	for_each(groups_sysfs->chips) {
+		struct gpio_chip *chip;
+		chip = groups_sysfs->chips[0];
+		chip->test(chip, chip->base);
+		chip->test(chip, chip->base + chip->ngpio - 1);
+		chip->test(chip, random(chip->base, chip->base + chip->ngpio - 1));
+	}
+//	printf("%-10s %-5s %-5s\n", name, base, ngpio);
+//	gpiochip=`ls -d $GPIO_DRV_SYSFS/gpiochip* 2>/dev/null`
+//	if [ X"$gpiochip" = X ]; then
+//		echo "no gpiochip"
+//	else
+//		for chip in $gpiochip; do
+//			name=`basename $chip`
+//			base=`cat $chip/base`
+//			ngpio=`cat $chip/ngpio`
+//			printf "%-10s %-5s %-5s\n" $name $base $ngpio
+//			if [ $ngpio = "0" ]; then
+//				echo "number of gpio is zero is not allowed".
+//			fi
+//			test_one_pin $base
+//			test_one_pin $(($base + $ngpio - 1))
+//			test_one_pin $((( RANDOM % $ngpio )  + $base ))
+//			if [ X$invalid_pin != X ]; then
+//				test_one_pin_fail $invalid_pin
+//			fi
+//		done
+//	fi
+	system(remove_module);
 }
 
 
@@ -131,6 +155,10 @@ int main(int argc, char *argv[])
 	struct gpio_testcase_t *t;
 	int i, j;
 
+	//TODO: paramter:
+	//-m module_name
+	//-t "force one test case"
+	//-s "force use sysfs interface no matter char device exist or not".
 	prerequisite();
 	for( i = 0; i < sizeof(gpio_testcases) / sizeof(struct gpio_testcase_t); i++) {
 		printf("%d\n", i);
