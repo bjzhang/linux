3320505dccb7b69c0b8c6ef1974948613f9d0a14
Yury Norov <ynorov@caviumnetworks.com>
2016-05-24 03:04:43 +0300

    arm64: ilp32: add is_ilp32_compat_{task,thread} and TIF_32BIT_AARCH64
    ILP32 tasks are needed to be distinguished from lp64 and aarch32.
    This patch adds helper functions is_ilp32_compat_{task,thread} and
    thread flag TIF_32BIT_AARCH64 to address it. This is a preparation
    for following patches in ilp32 patchset.
    
    For consistency, SET_PERSONALITY is changed here accordingly.
    
    Signed-off-by: Andrew Pinski <Andrew.Pinski@caviumnetworks.com>
    Signed-off-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
    Signed-off-by: Christoph Muellner <christoph.muellner@theobroma-systems.com>
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>
    Reviewed-by: David Daney <ddaney@caviumnetworks.com>

--- arch/arm64/include/asm/elf.h
+++ arch/arm64/include/asm/elf.h
@@ -142,11 +142,13 @@ typedef struct user_fpsimd_state elf_fpregset_t;
  */
 #define ELF_PLAT_INIT(_r, load_addr)	(_r)->regs[0] = 0
 
-#define SET_PERSONALITY(ex)						\
-({									\
+#define SET_PERSONALITY(ex)		\
+do {						\
 	clear_bit(TIF_32BIT, &current->mm->context.flags);		\
-	clear_thread_flag(TIF_32BIT);					\
-})
+	clear_thread_flag(TIF_32BIT);		\
+	clear_thread_flag(TIF_32BIT_AARCH64);	\
+	clear_bit(TIF_32BIT_AARCH64, &current->mm->context.flags);	\
+} while (0)
 
 /* update AT_VECTOR_SIZE_ARCH if the number of NEW_AUX_ENT entries changes */
 #define ARCH_DLINFO							\
@@ -187,11 +189,20 @@ typedef compat_elf_greg_t		compat_elf_gregset_t[COMPAT_ELF_NGREG];
 					 ((x)->e_flags & EF_ARM_EABI_MASK))
 
 #define compat_start_thread		compat_start_thread
+<<<<<<< HEAD
 #define COMPAT_SET_PERSONALITY(ex)					\
 ({									\
 	set_bit(TIF_32BIT, &current->mm->context.flags);		\
 	set_thread_flag(TIF_32BIT);					\
  })
+=======
+#define COMPAT_SET_PERSONALITY(ex)		\
+do {						\
+	clear_thread_flag(TIF_32BIT_AARCH64);	\
+	set_thread_flag(TIF_32BIT);		\
+} while (0)
+
+>>>>>>> 4ab73c15ab40... arm64: ilp32: add is_ilp32_compat_{task,thread} and TIF_32BIT_AARCH64
 #define COMPAT_ARCH_DLINFO
 extern int aarch32_setup_vectors_page(struct linux_binprm *bprm,
 				      int uses_interp);
