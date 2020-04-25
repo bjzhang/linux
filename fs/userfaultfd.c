d85b445972af18d8d2995f2f6df81903f4633c8b
Bamvor Zhang <bamv2005@gmail.com>
2020-04-25 15:18:51 +0800

    Revert "mm: comments for userfaultfd"
    This reverts commit 0e382f6875ed14dbd4f3c8c1ac493f3b1f211de6.

--- fs/userfaultfd.c
+++ fs/userfaultfd.c
@@ -1222,10 +1222,12 @@ static void __wake_userfault(struct userfaultfd_ctx *ctx,
 	spin_lock(&ctx->fault_pending_wqh.lock);
 	/* wake all in the range and autoremove */
 	if (waitqueue_active(&ctx->fault_pending_wqh)) {
+		pr_info("wake userfault to fault_pending_wqh\n");
 		__wake_up_locked_key(&ctx->fault_pending_wqh, TASK_NORMAL,
 				     range);
 	}
 	if (waitqueue_active(&ctx->fault_wqh)) {
+		pr_info("wake userfault to fault_wqh\n");
 		__wake_up(&ctx->fault_wqh, TASK_NORMAL, 1, range);
 	}
 	spin_unlock(&ctx->fault_pending_wqh.lock);
@@ -1739,6 +1741,7 @@ static int userfaultfd_copy(struct userfaultfd_ctx *ctx,
 		wake_userfault(ctx, &range);
 	}
 	ret = range.len == uffdio_copy.len ? 0 : -EAGAIN;
+	pr_info("userfaultfd_copy ret: %d\n", ret);
 out:
 	return ret;
 }
